ac3c1a3f4809c398e34917788444982dfc20c61d
Jeramie <jeramieramsantos79@gmail.com>
2026-03-30 20:40:07 +0800

    added API and initial fetch

--- react-weather-app/src/App.jsx
+++ react-weather-app/src/App.jsx
@@ -1,13 +1,28 @@
 import Search from "./Components/Search";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 
 const App = () => {
 
-  const [search, setSearch] = useState('');
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  const [search, setSearch] = useState('Philippines');
+  const [weather, setWeather] = useState([]);
 
   const handleChange = (e) => {
     setSearch(e.target.value)
   }
+
+  useEffect(() => {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=${API_KEY}`)
+      .then(response => {
+        setWeather(weather)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, [])
+
 
   return (
     <div className="main-container">
